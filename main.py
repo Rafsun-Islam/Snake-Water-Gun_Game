3d4c2f27874c591fa70fc430d262bb07d06c5a79
Rafsun Islam Taskin <rafsunislamtaskin024@gmail.com>
2025-04-18 13:25:22 +0600

    updated logic

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 import random
 
+# Randomly select the computer's choice
+computer = random.choice([-1, 0, 1])
 
-computer = random.choice([-1,0,1])
+# Dictionary for user input
+youDict = {'s': 1, 'w': -1, 'g': 0}
 
-youDict = {'s' : 1, 'w' : -1 , 'g' : 0}
+# Reverse dictionary for showing results
+revDict = {1: "Snake", -1: "Water", 0: "Gun"}
 
-revDict = {1 : "Snake" , -1 : "Water" , 0 : "Gun"}
+# Get user's choice
+youChoice = input("Enter Your Choice (s = Snake, w = Water, g = Gun): ").lower()
 
-youChoice = input("Enter Your Choice : ")
-
+# Convert the user's choice to corresponding integer value
 you = youDict[youChoice]
 
-print(f"You Chose {revDict[you]} and Computer Chose {revDict[computer]} ")
+# Print the choices
+print(f"You Chose {revDict[you]} and Computer Chose {revDict[computer]}")
 
-if(computer == you):
+# Check for draw
+if computer == you:
     print("It is a DRAW!")
 else:
-    
-    if(computer == 1 and you == 0):
+    # Check the win/loss conditions
+    if (computer == 1 and you == 0) or (computer == -1 and you == 1) or (computer == 0 and you == -1):
         print("You Win!")
-    elif(computer == -1 and you == 1):
-        print("You Win!")
-    elif(computer == 0 and you == -1):
-        print("You Win!")
-    elif(computer == 0 and you == 1):
-        print("You Loss!")
-    elif(computer == 1 and you == -1):
-        print("You Loss!")
-    elif(computer == -1 and you == 0):
+    elif (computer == 0 and you == 1) or (computer == 1 and you == -1) or (computer == -1 and you == 0):
         print("You Loss!")
     else:
-        print("Something went Wrong!")
+        print("Something went wrong!")
